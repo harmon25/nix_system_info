633caef0eb9db44deea7a23ca295009b800be6c9
harmoN <dougwright1@gmail.com>
2015-11-26 00:58:55 -0500

    moree

--- lib/pi_stats.ex
+++ lib/pi_stats.ex
@@ -1,20 +1,21 @@
 defmodule PiStats do
  use Application
 
- def get_hostname() do
+ def hostname() do
   result =  Porcelain.exec("hostname", ["-f"])
-  IO.inspect String.strip(result.out)
+  %{hostname: String.strip(result.out)}
  end
 
- def get_connections() do
+ def connections() do
   result = Porcelain.shell("netstat -nta --inet | wc -l")
-  IO.inspect String.strip(result.out)
+  IO.inspect 
+  %{connections: String.strip(result.out)}
  end
  
  def net_stats(int) do
    result = Porcelain.shell("/sbin/ifconfig #{int} | grep RX\\ bytes")
    result_tuple = List.to_tuple(String.split(String.replace(result.out,"bytes:","")))
-   data = %{:down => String.to_integer(elem(result_tuple, 1)), :up => String.to_integer(elem(result_tuple, 5)), :total => 0} 
+   data = %{down: String.to_integer(elem(result_tuple, 1)), up: String.to_integer(elem(result_tuple, 5)), total: 0} 
    %{data | :total => data.down + data.up }
  end
 
@@ -23,7 +24,6 @@ defmodule PiStats do
    split_result = 
      result.out
      |> String.strip()
-     |> String.replace("load average:", "")
      |> String.split(",")   
    result_tpl = List.to_tuple(split_result)
    tuple_len = tuple_size(result_tpl)
