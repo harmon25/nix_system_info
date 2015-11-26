cb74a2b4daa1d00978b5ac0def796ed9daa1d7ed
harmoN <dougwright1@gmail.com>
2015-11-26 18:04:40 -0500

    work on some error handling...

--- lib/sys_info.ex
+++ lib/sys_info.ex
@@ -3,6 +3,7 @@ defmodule SysInfo do
   Provides Pi System Stats
   """
   use Application
+  @genericError "Hmm sorry, not *nix"
 
   @doc """
   Returns hostname as string
@@ -13,10 +14,17 @@ defmodule SysInfo do
       {:ok, "hostname"}
   
   """
+
   def hostname() do
-    result =  Porcelain.exec("hostname", ["-f"])
-    final_result = result.out |> String.strip()
-    {:ok, final_result}
+    if OSUtils.is_unix? do
+      result =  Porcelain.exec("hostname", ["-f"])
+      final_result = result.out |> String.strip()
+      {:ok, final_result}
+    else
+      result =  Porcelain.exec("hostname", [])
+      final_result = result.out |> String.strip()
+      {:ok, final_result}
+    end
   end
   
   @doc """
@@ -29,11 +37,15 @@ defmodule SysInfo do
 
   """
   def connections() do
-    result = Porcelain.shell("netstat -nta --inet | wc -l")
-    final_result = result.out |> String.strip() |> String.to_integer()
-    {:ok, final_result}
+    if OSUtils.is_unix? do
+      result = Porcelain.shell("netstat -nta --inet | wc -l")
+      final_result = result.out |> String.strip() |> String.to_integer()
+      {:ok, final_result}
+    else
+       {:err, %{msg: @genericError }}
+    end
   end
- 
+
   @doc """
   Returns number of current tcp connetions as an int
 
@@ -43,15 +55,19 @@ defmodule SysInfo do
       {:ok, %{down: INT, up: INT, total: INT}}
   
   """
-  def net_stats(interface \\ "eth0") do
-    result = Porcelain.shell("/sbin/ifconfig #{interface} | grep RX\\ bytes")
-    result_tuple = result.out
-      |> String.replace("bytes:","")
-      |> String.split()
-      |> List.to_tuple()
+  def net_stats(interface \\ "eth0") when is_binary(interface) do
+    if OSUtils.is_unix? do
+      result = Porcelain.shell("/sbin/ifconfig #{interface} | grep RX\\ bytes")
+      result_tuple = result.out
+        |> String.replace("bytes:","")
+        |> String.split()
+        |> List.to_tuple()
 
-    data = %{down: String.to_integer(elem(result_tuple, 1)), up: String.to_integer(elem(result_tuple, 5)), total: 0} 
-    {:ok, %{data | :total => data.down + data.up } }
+      data = %{down: String.to_integer(elem(result_tuple, 1)), up: String.to_integer(elem(result_tuple, 5)), total: 0} 
+      {:ok, %{data | :total => data.down + data.up } }
+    else
+        {:err, %{msg: @genericError}}
+    end
   end
 
   @doc """
@@ -64,24 +80,28 @@ defmodule SysInfo do
 
   """
   def uptime() do
-   result =  Porcelain.shell("uptime")
-   result_tpl = result.out |> String.strip() |> String.split(",") |> List.to_tuple()  
-   tuple_len = tuple_size(result_tpl)
-    cond do
-      tuple_len == 5 ->
-      # up for hours
-        hours = elem(result_tpl, 0) |> String.split() |> List.last()
-      {:ok , %{hours: hours} }
-      tuple_len == 6 ->
-      # up for days
-        hours = elem(result_tpl, 1) |> String.strip()
-        days = elem(result_tpl, 0)
-          |> String.replace("days", "")
-          |> String.split()
-          |> List.last()
-          |> String.strip()
-        final_result = %{hours: hours, days: days }
-        {:ok, final_result}
+    if OSUtils.is_unix? do
+     result =  Porcelain.shell("uptime")
+     result_tpl = result.out |> String.strip() |> String.split(",") |> List.to_tuple()  
+     tuple_len = tuple_size(result_tpl)
+      cond do
+        tuple_len == 5 ->
+        # up for hours
+          hours = elem(result_tpl, 0) |> String.split() |> List.last()
+        {:ok , %{hours: hours} }
+        tuple_len == 6 ->
+        # up for days
+          hours = elem(result_tpl, 1) |> String.strip()
+          days = elem(result_tpl, 0)
+            |> String.replace("days", "")
+            |> String.split()
+            |> List.last()
+            |> String.strip()
+          final_result = %{hours: hours, days: days }
+          {:ok, final_result}
+      end
+    else
+       {:err, %{msg: @genericError}}
     end  
   end
 
@@ -95,6 +115,7 @@ defmodule SysInfo do
 
   """
   def load() do
+   if OSUtils.is_unix? do
     result =  Porcelain.shell("uptime")
     result_tpl = result.out
       |> String.strip()
@@ -105,6 +126,9 @@ defmodule SysInfo do
     [load_1,load_5,load_15] = [String.strip(elem(result_tpl, tuple_len-3)),String.strip(elem(result_tpl, tuple_len-2)), String.strip(elem(result_tpl,tuple_len-1))] 
     final_result = %{load_1: String.to_float(load_1) , load_5: String.to_float(load_5) , load_15: String.to_float(load_15) }
     {:ok, final_result }
+   else
+       {:err, %{msg: @genericError}}
+   end
   end
 
   @doc """
@@ -117,6 +141,7 @@ defmodule SysInfo do
 
   """
   def disks() do
+   if OSUtils.is_unix? do
     res = Porcelain.shell("df -T | grep -vE \"tmpfs|rootfs|Filesystem\"")
     disk_stats = res.out 
       |> String.strip()
@@ -124,6 +149,10 @@ defmodule SysInfo do
       |> Enum.map(fn x -> 
           String.split(x) |> List.to_tuple()
          end)
+      {:ok, disk_stats }
+    else
+       {:err, %{msg: @genericError}}
+    end
   end
 
   @doc """
@@ -136,10 +165,13 @@ defmodule SysInfo do
 
   """
   def memory() do 
-    result =  Porcelain.exec("free", ["-mo"])
-    result_tpl = result.out |> String.strip() |> String.split("\n") |> Enum.map(fn x -> String.split(x) end) |> tl() |> hd() |> List.to_tuple()
-    final_result = %{free: String.to_integer(elem(result_tpl,3)), total: String.to_integer(elem(result_tpl,1)), used: String.to_integer(elem(result_tpl,2)) }
-    {:ok, final_result }
+    if OSUtils.is_unix? do
+      result =  Porcelain.exec("free", ["-mo"])
+      result_tpl = result.out |> String.strip() |> String.split("\n") |> Enum.map(fn x -> String.split(x) end) |> tl() |> hd() |> List.to_tuple()
+      final_result = %{free: String.to_integer(elem(result_tpl,3)), total: String.to_integer(elem(result_tpl,1)), used: String.to_integer(elem(result_tpl,2)) }
+      {:ok, final_result }
+    else
+      {:err, %{msg: @genericError}}
+    end
   end
-
 end
