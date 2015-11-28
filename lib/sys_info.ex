35126a885cae793775efa732b8dd4caa44650d20
harmoN <dougwright1@gmail.com>
2015-11-27 21:08:09 -0500

    hmm go back...

--- lib/sys_info.ex
+++ lib/sys_info.ex
@@ -5,20 +5,57 @@ defmodule SysInfo do
   use Application
   @genericError "Hmm sorry, not *nix"
 
-  def hostname() do
-   :inet.gethostname()
-  end
+  @doc """
+  Returns hostname as string
+
+  ## Examples:
+    
+      iex> SysInfo.hostname
+      {:ok, "hostname"}
   
-  defp connections() do
-   get() do
-      result = Porcelain.shell("netstat -nta --inet | wc -l")
-      final_result = result.out |> String.strip() |> String.to_integer()
+  """
+  def hostname() do
+    if OSUtils.is_unix? do
+      result =  Porcelain.exec("hostname", ["-f"])
+      final_result = result.out |> String.strip()
+      {:ok, final_result}
+    else
+      result =  Porcelain.exec("hostname", [])
+      final_result = result.out |> String.strip()
       {:ok, final_result}
     end
   end
+  
+  @doc """
+  Returns number of current tcp connetions as an int
 
-  defp net_stats(interface \\ "eth0") when is_binary(interface) do
-    get() do
+  ## Examples:
+
+      iex> SysInfo.connections
+      {:ok, INT}
+
+  """
+  def connections() do
+    if OSUtils.is_unix? do
+      result = Porcelain.shell("netstat -nta --inet | wc -l")
+      final_result = result.out |> String.strip() |> String.to_integer()
+      {:ok, final_result}
+    else
+       {:err, %{msg: @genericError }}
+    end
+  end
+
+  @doc """
+  Returns number of current tcp connetions as an int
+
+  ## Examples:
+
+      iex> SysInfo.net_stats "wlan0"
+      {:ok, %{down: INT, up: INT, total: INT}}
+  
+  """
+  def net_stats(interface \\ "eth0") when is_binary(interface) do
+    if OSUtils.is_unix? do
       result = Porcelain.shell("/sbin/ifconfig #{interface} | grep RX\\ bytes")
       result_tuple = result.out
         |> String.replace("bytes:","")
@@ -30,11 +67,22 @@ defmodule SysInfo do
                total: 0} 
 
       {:ok, %{data | :total => data.down + data.up } }
+    else
+        {:err, %{msg: @genericError}}
     end
   end
 
-  defp uptime() do
-    get() do
+  @doc """
+  Uptime as Map
+
+  ## Examples:
+
+      iex> SysInfo.uptime
+      {:ok, %{s: seconds, m: minutes, d: days, h:hours, y: years}}
+
+  """
+  def uptime() do
+    if OSUtils.is_unix? do
      result =  Porcelain.shell("cat /proc/uptime")
      uptime = result.out 
         |> String.split()
@@ -43,72 +91,94 @@ defmodule SysInfo do
         |> elem(0)
         |> convertUptime()
       {:ok,  uptime}  
+    else
+       {:err, %{msg: @genericError}}
     end  
   end
 
-  defp load() do
-   get() do
+  @doc """
+  Returns load averages for last 1, 5 and 15 minutes as floats
+
+  ## Examples:
+
+      iex> SysInfo.load
+      {:ok, %{load_1: a, load_5: b, load_15: c} }
+
+  """
+  def load() do
+   if OSUtils.is_unix? do
     result =  Porcelain.shell("uptime")
     result_tpl = result.out
       |> String.strip()
       |> String.replace("load average:", "")
       |> String.split(",")
       |> List.to_tuple()
+      
     tuple_len = tuple_size(result_tpl)
 
-<<<<<<< HEAD
-    {:ok, 
-    %{load_1: String.strip(elem(result_tpl, tuple_len-3)), 
-      load_5: String.strip(elem(result_tpl, tuple_len-2)),  
-      load_15: String.strip(elem(result_tpl,tuple_len-1))
-      }     
-    }
-=======
-    [load_1,load_5,load_15] = [String.strip(elem(result_tpl, tuple_len-3)), 
-                               String.strip(elem(result_tpl, tuple_len-2)), 
-                               String.strip(elem(result_tpl,tuple_len-1))] 
-
-    final_result = %{load_1: Float.round(String.to_float(load_1)), 
-                     load_5: Float.round(String.to_float(load_5)), 
-                     load_15: Float.round(String.to_float(load_15))
+    final_result = %{load_1: String.strip(elem(result_tpl, tuple_len-3)), 
+                     load_5: String.strip(elem(result_tpl, tuple_len-2)),  
+                     load_15: String.strip(elem(result_tpl,tuple_len-1))
                     }
     {:ok, final_result }
    else
        {:err, %{msg: @genericError}}
->>>>>>> parent of 0e0df68... fix load
    end
   end
 
-  defp disks() do
-    get() do
-      res = Porcelain.shell("df -T | grep -vE \"tmpfs|rootfs|Filesystem\"")
-      disk_stats = res.out 
-        |> String.strip()
-        |> String.split("\n")
-        |> Enum.map(fn x -> 
-            String.split(x) |> List.to_tuple()
-           end)
-        |> Enum.map(fn diskInfo ->
-            %{device: elem(diskInfo,0),
-              type: elem(diskInfo,1), 
-              size: String.to_integer(elem(diskInfo,2)), 
-              capacity: elem(diskInfo,5), 
-              mountpoint: elem(diskInfo,6)
-             }
-           end)
+  @doc """
+  Returns List of mounted file systems
+
+  ## Examples:
+
+      iex> SysInfo.disks
+      {:ok, []}
+
+  """
+  def disks() do
+   if OSUtils.is_unix? do
+    res = Porcelain.shell("df -T | grep -vE \"tmpfs|rootfs|Filesystem\"")
+    disk_stats = res.out 
+      |> String.strip()
+      |> String.split("\n")
+      |> Enum.map(fn x -> 
+          String.split(x) |> List.to_tuple()
+         end)
+      |> Enum.map(fn diskInfo ->
+          %{device: elem(diskInfo,0),
+            type: elem(diskInfo,1), 
+            size: String.to_integer(elem(diskInfo,2)), 
+            capacity: elem(diskInfo,5), 
+            mountpoint: elem(diskInfo,6)
+           }
+         end)
       {:ok, disk_stats }
+    else
+       {:err, %{msg: @genericError}}
     end
   end
 
-  defp memory() do 
-     get() do
-        result =  Porcelain.exec("free", ["-mo"])
-        result_tpl = result.out |> String.strip() |> String.split("\n") |> Enum.map(fn x -> String.split(x) end) |> tl() |> hd() |> List.to_tuple()
-        {:ok, %{free: String.to_integer(elem(result_tpl,3)), 
-          total: String.to_integer(elem(result_tpl,1)),
-          used: String.to_integer(elem(result_tpl,2))
-         } }
-      end
+  @doc """
+  Returns Map of memory stats as integers
+  
+  ## Examples:
+
+      iex> SysInfo.memory
+      {:ok, %{free: a, total: b, used: c}}
+
+  """
+  def memory() do 
+    if OSUtils.is_unix? do
+      result =  Porcelain.exec("free", ["-mo"])
+      result_tpl = result.out |> String.strip() |> String.split("\n") |> Enum.map(fn x -> String.split(x) end) |> tl() |> hd() |> List.to_tuple()
+      final_result = %{free: String.to_integer(elem(result_tpl,3)), 
+                       total: String.to_integer(elem(result_tpl,1)),
+                       used: String.to_integer(elem(result_tpl,2))
+                      }
+      {:ok, final_result }
+    else
+      {:err, %{msg: @genericError}}
+    end
   end
 
   defp convertUptime(seconds) do
@@ -119,40 +189,6 @@ defmodule SysInfo do
       m: (seconds / 60) |> round() |> rem(60), 
       s: seconds |> rem(60)
      }
-  end
-
-
-  @doc """
-    Return system info, accepts an atom as tje first argument
-    Followed by optional arguments
-    :memory
-    :uptime
-    :load
-    :disks
-    :net_stats, "eth0"
-    :connections
-  
-  ## Examples:
-
-      iex> SysInfo.memory
-      {:ok, %{free: a, total: b, used: c}}
-
-  """
-
-  def get(block) do
-     if OSUtils.is_unix? do
-       block.()
-     else
-        {:err, %{msg: @genericError}}
-     end
-  end
-
-  def get(block, opt) do
-     if OSUtils.is_unix? do
-       block.(opt)
-     else
-        {:err, %{msg: @genericError}}
-     end
   end
 
 end
